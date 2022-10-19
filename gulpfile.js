a6a26963c43ef10f6e14e570c555717106bcc825
Lim137 <sergei.ignatov2001@inbox.ru>
2022-10-19 16:15:38 +0300

    added server

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ global.app = {
 import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
 import { html } from "./gulp/tasks/html.js";
+import {server} from "./gulp/tasks/server.js";
 
 
 function watcher() {
@@ -17,5 +18,5 @@ gulp.watch(path.watch.files, copy);
 gulp.watch(path.watch.html, html);
 }
 const mainTasks = gulp.parallel(copy, html)
-const dev = gulp.series(reset, mainTasks, watcher);
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher,server));
 gulp.task('default', dev);
